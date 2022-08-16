af5d679b6d0fe451a5bb668e068ca6b2bb5b6ad3
Carol Maser <92386477+CarolMaser@users.noreply.github.com>
2022-08-16 09:04:59 -0300

    Antecessor e Sucessor
    Achando o antecessor e o sucessor de um número digitado pelo usuário.

---
+++ pbm01/ex03/ex03.php
@@ -0,0 +1,10 @@
+<?php
+
+    $num = $_GET["num"];
+    $sub = $num - 1;
+    $soma = $num + 1;
+
+    echo "O antecessor de " . $num . " é o valor " . $sub;
+    echo "<br>O sucessor de " . $num . " é o valor " . $soma;
+    
+?>
